c4b2d1943c5dea82e735ebbac6bc9443843c2a11
sadfaerie <sadfaerie@gmail.com>
2018-11-07 10:08:52 +0000

    added flask

--- run.py
+++ run.py
@@ -1,19 +1,40 @@
 import os
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", page_title="Welcome")
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company_data=data)
 
-@app.route("/contact")
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data= json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    #return "<h1>" + member['name'] + "</h1>"
+    return render_template("member.html", member=member)
+
+@app.route("/contact", methods=["GET","POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        print("Hello? Hello?")
+    return render_template("contact.html", page_title="Contact")
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
